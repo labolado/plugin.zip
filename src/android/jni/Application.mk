e2e2f0679d47f85fdd9d8196e30c38a979b44a26
Vlad Shcherban <vlad@coronalabs.com>
2022-06-27 07:30:44 -0400

    rebuilding zip with old toolchain

--- src/android/jni/Application.mk
+++ src/android/jni/Application.mk
@@ -6,5 +6,5 @@ APP_PLATFORM := android-15
 APP_ABI := armeabi-v7a x86 x86_64 arm64-v8a
 
 # Box2D needs this
-APP_STL := c++_static
-# APP_STL := stlport_static
+# APP_STL := c++_static
+APP_STL := stlport_static
